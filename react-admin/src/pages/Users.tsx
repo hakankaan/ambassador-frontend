a682285a7064705040e20bcbbc91c59315e27dd0
Kaan Hakan <kaanhakan2005@gmail.com>
2021-11-09 19:52:02 +0300

    feat: added pagination to table

--- react-admin/src/pages/Users.tsx
+++ react-admin/src/pages/Users.tsx
@@ -1,17 +1,29 @@
-import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import { User } from "../models/user";
 
 const Users = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(5)
+
   useEffect(() => {
     (async () => {
       const { data } = await axios.get("ambassadors");
       setUsers(data);
     })();
   }, []);
+
   return (
     <Layout>
       <Table>
@@ -24,12 +36,13 @@ const Users = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {users.map((user) => {
+          {users.slice(page * rowsPerPage, (page + 1) * rowsPerPage).map((user) => {
             return (
               <TableRow key={user.id}>
                 <TableCell>{user.id}</TableCell>
                 <TableCell>
-                  {user.first_name} {user.last_name}
+                  {user.first_name}{" "}
+                  {user.last_name}
                 </TableCell>
                 <TableCell>{user.email}</TableCell>
                 <TableCell></TableCell>
@@ -37,6 +50,22 @@ const Users = () => {
             );
           })}
         </TableBody>
+        <TableFooter>
+          <TablePagination
+            component="div"
+            count={users.length}
+            page={page}
+            onPageChange={(
+              event: MouseEvent<HTMLButtonElement> | null,
+              newPage: number
+            ) => setPage(newPage)}
+            rowsPerPage={rowsPerPage}
+            onRowsPerPageChange={(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+              setRowsPerPage(parseInt(event.target.value, 10))
+            }}
+            rowsPerPageOptions={[5, 10, 20]}
+          />
+        </TableFooter>
       </Table>
     </Layout>
   );
